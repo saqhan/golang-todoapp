add3a49c35dbc5d0a8a8111fd03d8d1461585aeb
saqhan <saqhanshagaev@MacBook-Pro-saqhan.local>
2026-05-09 15:03:48 +0300

    infra setup

---
+++ migrations/000001_init.up.sql
@@ -0,0 +1,29 @@
+CREATE SCHEMA todoapp;
+
+CREATE TABLE todoapp.users (
+    id              SERIAL                   PRIMARY KEY,
+    version         BIGINT          NOT NULL DEFAULT 1,
+    full_name       VARCHAR(100)    NOT NULL CHECK (char_length(full_name) BETWEEN 3 AND 100),
+    phone_number    VARCHAR(15)              CHECK (
+        phone_number ~ '^\+[0-9]+$'
+        and
+        char_length(phone_number) BETWEEN 10 AND 15
+    )
+);
+
+CREATE TABLE todoapp.tasks (
+    id                  SERIAL                   PRIMARY KEY,
+    version             BIGINT          NOT NULL DEFAULT 1,
+    title               VARCHAR(100)    NOT NULL CHECK (char_length(title) BETWEEN 1 AND 100),
+    description         VARCHAR(255),            CHECK (char_length(description) BETWEEN 1 AND 1000),
+    completed           BOOLEAN         NOT NULL DEFAULT FALSE,
+    created_at          TIMESTAMPTZ     NOT NULL DEFAULT NOW(),
+    completed_at        TIMESTAMPTZ,
+
+    CHECK (
+        (completed = FALSE AND completed_at IS NULL) OR
+        (completed = TRUE AND completed_at IS NOT NULL AND completed_at >= created_at)
+    ),
+
+    author_user_id      INTEGER         NOT NULL REFERENCES todoapp.users(id)
+);
